dc39cf7ed69737264a70aadeb6ed1a51231e170b
global2alex <global2alex@gmail.com>
2019-04-15 22:59:59 -0400

    Switched argparse for click

--- poetry_slug/application.py
+++ poetry_slug/application.py
@@ -1,32 +1,13 @@
-from argparse import ArgumentParser
+import click
 
-class Application:
-  def __init__(self):
-    self.user_input = self.__parse_args__()
+@click.group()
+def Application():
+  """
+  Just a test, hello?
+  """
 
-    if self.user_input.mass_compare:
-      print("Mass Compare!")
-    elif self.user_input.compare:
-      print("Comapre Individual Files")
-
-  def __parse_args__(self):
-    parser = ArgumentParser(
-      prog="python main.py",
-      description="""
-      This tool helps... maybe
-      """,
-    )
-    parser.add_argument(
-      "-c",
-      "--compare",
-      type=str,
-      default=False,
-      help="Yup, that's what this does."
-    )
-    parser.add_argument(
-      "-m",
-      "--mass-compare",
-      action="store_true",
-      help="... I have no idea... ???"
-    )
-    return parser.parse_args()
+@Application.command('compare', short_help='Number of greetings.')
+def cli(count):
+  """
+  test2
+  """
