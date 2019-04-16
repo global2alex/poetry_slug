bcdee5c0fffa6470a2ff15e2ac65ce5653f51a9f
global2alex <global2alex@gmail.com>
2019-04-15 21:42:10 -0400

    Added: args

--- poetry_slug/__init__.py
+++ poetry_slug/__init__.py
@@ -1,1 +1,5 @@
 __version__ = '0.1.0'
+
+from .application import Application
+
+Application()
---
+++ poetry_slug/application.py
@@ -0,0 +1,32 @@
+from argparse import ArgumentParser
+
+class Application:
+  def __init__(self):
+    self.user_input = __parse_args__()
+
+    if self.user_input.mass_compare:
+      print("Mass Compare!")
+    elif self.user_input.compare:
+      print("Comapre Individual Files")
+
+  def __parse_args__():
+    parser = ArgumentParser(
+      prog="python main.py",
+      description="""
+      This tool helps... maybe
+      """,
+    )
+    parser.add_argument(
+      "-c",
+      "--compare",
+      type=str,
+      default=False,
+      help="Yup, that's what this does."
+    )
+    parser.add_argument(
+      "-m",
+      "--mass-compare",
+      action="store_true",
+      help="... I have no idea... ???"
+    )
+    return parser.parse_args()
